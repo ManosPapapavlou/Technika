391aba7d51058c9fc0983e1fd32616e132dd70f7
ManosPapapavlou <94015114+ManosPapapavlou@users.noreply.github.com>
2024-01-16 15:32:07 +0200

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -224,8 +224,22 @@
       clickable: true
     }
   });
+  
+  /**
+   * Animation on scroll
+   */
   window.addEventListener('load', () => {
-    aos_init();
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
   });
-/**
-)()
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})()
